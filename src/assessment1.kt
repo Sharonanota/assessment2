1cb04f804fc8354bec56a20e3ba9247ea4416fd3
Sharonanota <matakwamatakwa@gmail.com>
2022-03-10 08:53:36 +0300

    first commit

---
+++ src/assessment1.kt
@@ -0,0 +1,29 @@
+fun main(){
+ //  var name=("codeHive")
+   // println(name[5])
+  //  println(name[6])
+  //  println(name[7])
+   // println(name[8])
+
+var s=numbers(65,63)
+println(s)
+
+    var r=("Sharon")
+    println(r.length)
+    var u=fact(23)
+    println(u)
+}
+
+
+fun numbers(E:Int,c:Int):Int{
+    var n=(E%c)
+    return n
+}
+fun fact(I:Int){
+    var r=("Hi my name is Sharon I am "+""+I +""+ " years old" )
+    println(r)
+}
+
+fun count(e:String){
+var u=(e)
+}
